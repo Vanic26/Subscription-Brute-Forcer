5636077b6dc139b3301d6aa7e3e0d644d811387d
Vanic <jonny90.appleseed@mail.ru>
2026-09-27 01:01:07 +0630

    Update checking_inputs.ps1

--- .github/scripts/checking_inputs.ps1
+++ .github/scripts/checking_inputs.ps1
@@ -63,9 +63,9 @@ if (Test-Path $stateFile) {
 # ------------------------------------------------------------
 if ($env:GITHUB_EVENT_NAME_INPUT -eq "schedule") {
     Write-Host ""
+    Write-Host "============================================"
     Write-Host "[MODE] Scheduled run."
     Write-Host "[MODE] Loading latest inputs from last_runner_states.txt"
-    Write-Host ""
 
     if ([string]::IsNullOrWhiteSpace($saved.MODE)) {
         Write-Host "[ERROR] No saved MODE found in last_runner_states.txt."
